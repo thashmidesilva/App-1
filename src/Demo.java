5e04b542d612d3a030a66d1b7750749a0316f408
Thashmila <thashmilaaraliya@gmail.com>
2021-09-16 20:14:38 -0700

    2

--- src/Demo.java
+++ src/Demo.java
@@ -1,5 +1,18 @@
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+
 public class Demo {
     public static void main(String[] args) {
-
+        Student student = new Student("S001", "Nimal", "Colombo", "011");
+        //class
+        Configuration configuration=new Configuration() ;
+        //interface
+        SessionFactory sessionFactory= configuration.buildSessionFactory();
+        //interface
+        Session session=SessionFactory.openSession();
+        session.save(student);
     }
 }
+
